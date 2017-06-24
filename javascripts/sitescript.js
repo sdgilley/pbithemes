4dc982a7c1b97072cedc0ddeb7021cfa91d61876
Sheri Gilley <sgilley@microsoft.com>
2017-06-23 23:07:30 -0500

    updates

--- javascripts/sitescript.js
+++ javascripts/sitescript.js
@@ -3,7 +3,8 @@
 $(document).ready ( function () {
     //initialize
     //$('#inputDlg').modal('show');
-    var tmpl;
+    var theme; 
+    // load initial theme
     var inp = '{ \
         "name": "Waveform", \
         "dataColors": ["#31B6FD", "#4584D3", "#5BD078", "#A5D028", "#F5C040", "#05E0DB", "#3153FD", "#4C45D3", "#5BD0B0", "#54D028", "#D0F540", "#057BE0"], \
@@ -13,6 +14,7 @@ $(document).ready ( function () {
         }';
      $("#input").val( inp );
     
+    // selT dropdown on Load dialog pre-loads a theme
     $("#selT").change(function(){
         var t = $(this).val();
         switch(t) {
@@ -50,38 +52,43 @@ $(document).ready ( function () {
 
     });
   
-
+// loadnew button closes the dialog - now read the input to populate the main page.
     $("#loadnew").click(function(){
-        tmpl = $.parseJSON( $("#input").val() );
-        LoadColors(tmpl);
-        generate();
+        theme = $.parseJSON( $("#input").val() );
+        LoadColors(theme);  // creates the text input cells and colors them
+        generate(); //generates the json text from text input cells
+        $("#result").removeClass('hidden');
+
     });
 
-    function LoadColors(tmpl){        
-
-        $("#tmplName").html( "Template: <strong> " + tmpl.name + "</strong>" );
-
-        var bg = 'Background: <input type="text" id="bg" class="choose"  value=" ' + tmpl.background + '"/> &nbsp; &nbsp;';
-        var fg = 'Foreground: <input type="text"  id="fg"class="choose"  value=" ' + tmpl.foreground + '"/> &nbsp; &nbsp;';
-        var ta = 'Table accent: <input type="text" id="ta" class="choose"  value=" ' + tmpl.tableAccent + '"/>';
+    function LoadColors(theme){        
+        // creates the text input cells and uses value for background colorblind
+        // TODO: change text color when value is too dark
+        // TODO: add color picker for easier input
+        // TODO: currently no error checking.  bad colors won't change the current cell color. 
+        $("#themeName").html( "Theme: <strong> " + theme.name + "</strong>" );
+        // general section shows background, foreground, table accent
+        var bg = 'Background: <input type="text" id="bg" class="choose"  value=" ' + theme.background + '"/> &nbsp; &nbsp;';
+        var fg = 'Foreground: <input type="text"  id="fg"class="choose"  value=" ' + theme.foreground + '"/> &nbsp; &nbsp;';
+        var ta = 'Table accent: <input type="text" id="ta" class="choose"  value=" ' + theme.tableAccent + '"/>';
         $("#general").html (bg + fg + ta);
 
-        //data colors
+        //data colors section - build a cell for each value
         var dc = "Data Colors:</br>";
-        $.each(tmpl.dataColors, function (index, value) {
+        $.each(theme.dataColors, function (index, value) {
         dc = dc + '<input type="text" class="choose data"  value=" ' + value + '"/>'
         });
         $("#data").html (dc);
 
 
-        //apply the colors for newly generated items
+        //apply the background color to each cell
         $( ".choose" ).each(function() {
         $(this).css({"backgroundColor" : $(this).val() });
 
         //update cells each time user types new value
          $( ".choose" ).change(function() {
         $(this).css({"backgroundColor" : $(this).val() });
-        generate();
+        generate(); //regenerate final theme at each cell change
         });
 
     });
@@ -89,9 +96,9 @@ $(document).ready ( function () {
 };
 
     function generate (){
-        // generate new template based on current values
+        // generate new theme based on current values
         var newTmp = new Object();
-        newTmp.name = tmpl.name;
+        newTmp.name = theme.name;
         var dcs = [];
         $(".data").each(function() {
             dcs.push($(this).val());
