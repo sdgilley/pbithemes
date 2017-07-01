b411a56faffd8afae861075ab35c67d935c73536
Sheri Gilley <sgilley@microsoft.com>
2017-07-01 15:45:02 -0500

    another move

--- javascripts/sitescript.js
+++ javascripts/sitescript.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 
     // populate the theme selector
     $.ajax({
-        url: 'images/themes/',
+        url: 'images/',
         success: function (data) {
             //list all themes
             $(data).find("a:contains(" + fileExt + ")").each(function () {
@@ -18,7 +18,7 @@ $(document).ready(function () {
 
     // click on a file
     $("#files").change(function () {
-        filename = 'images/themes/' + $("#files").val()
+        filename = 'images/' + $("#files").val()
         $.get(filename, function (data) {
             $("#input").val(JSON.stringify(data));
             showstrip(data);
